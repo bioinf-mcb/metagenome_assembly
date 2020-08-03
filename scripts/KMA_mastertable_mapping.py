fc463ed080136dd4501682237b0445198f79a597
Marysteph <gathonimaranga@gmail.com>
2020-08-03 23:52:15 +0200

    added GO_CPM mapping script

--- scripts/KMA_mastertable_mapping.py
+++ scripts/KMA_mastertable_mapping.py
@@ -21,13 +21,12 @@ def load_normalized_kma_file(path):
 	"""
 	Reads KMA normalized depth file
 
-    Parameters
-    ----------
+	Parameters
+	----------
 	input_file : tsv
-        tsv file of kma normalized depths
-
-    Returns
-    -------
+	tsv file of kma normalized depths
+	Returns
+	-------
 	Pandas dataframe
 	"""
 	kma_df = pd.read_csv(path, sep="\t", skiprows=1, names=['Gene_ID', 'CPM'])
@@ -38,11 +37,11 @@ def load_genemapper_table(path):
 	"""
 	Reads gene mapping table
 	Parameters
-    ----------
+	----------
 	input_file : tsv
 
-    Returns
-    -------
+	Returns
+	-------
 	Pandas dataframe
 
 	"""
@@ -57,7 +56,7 @@ def load_genemapper_table(path):
               help='Input normalized KMA depth file .tsv file.')
 @click.option('--gene_mapper_file', '-g', required=True,
               type=click.Path(resolve_path=True, readable=True, exists=True),
-              help='Input path to gene mapping table .tsv file.')
+              help='Input gene mapping table .tsv file.')
 @click.option('--out_file', '-o', required=True,
               type=click.Path(resolve_path=True, readable=True, exists=False),
               help='Output .tsv file.')
@@ -81,12 +80,12 @@ def _perform_summing_up_CPM(kma_file, gene_mapper_file, out_file):
           .rename(columns={0:'GOs'})\
           .reset_index(drop=True)
 
-    # mapping kma dataframe to GO mapping df
+	# mapping kma dataframe to GO mapping df
 	cpm_gene_go = pd.merge(gene_go, kma_df,
 		left_on='Gene ID', right_on='Gene_ID',
 		how='outer')
     
-    # sum up CPMs per GO term
+	# sum up CPMs per GO term
 	CPM_per_GO = cpm_gene_go.groupby(['GOs'])['CPM'].agg(np.sum).reset_index()
 
 	# saving to file
